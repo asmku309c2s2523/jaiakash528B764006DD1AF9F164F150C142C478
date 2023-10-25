2c225ce69508391eed8325044680f25fe8544a5a
asmku309c2s2523 <144327671+asmku309c2s2523@users.noreply.github.com>
2023-10-25 14:26:56 +0530

    Add files via upload

---
+++ unit -3/Program_1.py
@@ -0,0 +1,19 @@
+
+# Define the linearSearchProduct function
+def linearSearchProduct(productList, targetProduct):
+    indices = []
+    
+    for index, product in enumerate(productList):
+        if product == targetProduct:
+            indices.append(index)
+    
+    return indices
+
+# Example usage
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+
+# Call the linearSearchProduct function and print the result
+result = linearSearchProduct(products, target)
+print(result)
